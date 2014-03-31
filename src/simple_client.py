e17912e4cbf342e4b021123316ef3aeac2e6ac62
Tommy Tracy II <tjt7a@virginia.edu>
2014-03-30 22:28:40 -0400

    Fixed cntrl-c bug

--- src/simple_client.py
+++ src/simple_client.py
@@ -1,19 +1,28 @@
 import socket
 import sys
 
+# Address of the server to connect to
 ADDRESS = ("localhost", 8080)
+
+# Data to send to the server (in this case, the command line arguments passed to the program)
 data = " ".join(sys.argv[1:])
 
+# Open a socket
 socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+
+# Attempt to connect to the server and send the data
 try:
 		socket.connect(ADDRESS)
 		socket.sendall(data+"\n")
 
+# Receive the reply
 		received = socket.recv(1024)
 
+# Finally, close the socket
 finally:
 		socket.close()
 
+# Print transaction
 print "Sent: {}".format(data)
 print "Received: {}".format(received)
--- src/simple_server.py
+++ src/simple_server.py
@@ -1,14 +1,26 @@
 import SocketServer
+import signal
+import sys
+
+# Handle Control-C Signal
+def signal_handler(signal, frame):
+	print('Killing Server...')
+	server.server_close()
+	sys.exit(0)
+
 
 # Simple handler that will accept the first chunk of data sent by the client
 class MyHandler(SocketServer.BaseRequestHandler):
 
+	# Grab the first chunk and print out what we got and return it
 	def handle(self):
 			self.data = self.request.recv(1024).strip()
 			print "{} wrote: ".format(self.client_address[0])
 			print self.data
 
-			self.request.sendall(self.data.upper())
+
+			self.request.sendall(self.data)
+
 
 # A stream handler calls recv() several times until it encounters a newline
 class MySecondHandler(SocketServer.StreamRequestHandler):
@@ -17,10 +29,17 @@ class MySecondHandler(SocketServer.StreamRequestHandler):
 			self.data = self.rfile.readline().strip()
 			print "{} wrote:".format(self.client_address[0])
 			print self.data
-			self.wfile.write(self.data.upper())
+
+			self.wfile.write(self.data)
 
 if __name__ == "__main__":
+
+	# To handle killing the server with Control-C
+	signal.signal(signal.SIGINT, signal_handler)
+
+	# Run server on port 8080
 	ADDRESS = ("localhost", 8080)
 
+	# Start server
 	server = SocketServer.TCPServer(ADDRESS, MySecondHandler)
 	server.serve_forever()
